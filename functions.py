894062cb8dc219f5ffe6a30e12de929b6c4e64b0
Njla1 <nicolasjoselevinalvarez@gmail.com>
2022-06-22 22:42:15 -0300

    v1.1.1
    Se agrego la documentacion de restarCuenta.
    Se modificaron los returns para que retorne tuplas de forma directa.

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ def retiros(div,sS,sP,psw):
     """
     Realiza un retiro de dinero desde una caja especificada en funcion de un tipo de moneda.
     Admite hasta dos ingresos de monto invalidos.
-    Retorna una tupla con los valores de saldo posteriores al procesamiento.
+    Retorna la tupla resultante de la funcion restarCuenta a la funcion principal.
     """
     first=True
     reingreso=0
@@ -27,26 +27,25 @@ def retiros(div,sS,sP,psw):
                 reingreso=int(input("No cuenta con saldo suficiente, desea reigresar el monto?\n1. si\n2. no\n"))
             else:
                 reingreso=2
-    nuevosaldo=[sP,sS]
-    return nuevosaldo
+    return sP, sS
 
 
 def consultas(div,sP,sS,hP,hS):
     """
     
     """
-    PGoM=int(input("que desea consultar:\n1.Posicion Global\n2.Movimientos\n"))
+    PGoM=int(input("Que desea consultar:\n1.Posicion Global\n2.Movimientos\n"))
     if PGoM<2:
-        divisa=int(input("que cuenta desea observar?\n1.pesos\n2.soles\n"))
+        divisa=int(input("Que cuenta desea observar?\n1.pesos\n2.soles\n"))
         if div<2:
-            print(f'su saldo en pesos es de ${sP}')
+            print(f'Su saldo en pesos es de ${sP}')
             reporte=sP
         else:
-            print(f'su saldo en soles es de ${sS}')
+            print(f'Su saldo en soles es de ${sS}')
             reporte=sS
     else:
         if div<2:
-            print(f'sus ultimos movimientos en su cuenta en pesos son {hP}')
+            print(f'sus ultimos movimientos en su cuenta en pesos son: {hP}')
             reporte=sP
         else:
             print(f'sus ultimos movimientos en su cuenta en soles son:{hS}')
@@ -54,44 +53,32 @@ def consultas(div,sP,sS,hP,hS):
     imprimir=int(input("desea imprimir un reporte?\n1. si\n2. no\n"))
     if imprimir<2:
         print("su reporte se esta imprimiendo...")
-        print("""———————————No recivo?———————————
-⠀⣞⢽⢪⢣⢣⢣⢫⡺⡵⣝⡮⣗⢷⢽⢽⢽⣮⡷⡽⣜⣜⢮⢺⣜⢷⢽⢝⡽⣝
-⠸⡸⠜⠕⠕⠁⢁⢇⢏⢽⢺⣪⡳⡝⣎⣏⢯⢞⡿⣟⣷⣳⢯⡷⣽⢽⢯⣳⣫⠇
-⠀⠀⢀⢀⢄⢬⢪⡪⡎⣆⡈⠚⠜⠕⠇⠗⠝⢕⢯⢫⣞⣯⣿⣻⡽⣏⢗⣗⠏⠀
-⠀⠪⡪⡪⣪⢪⢺⢸⢢⢓⢆⢤⢀⠀⠀⠀⠀⠈⢊⢞⡾⣿⡯⣏⢮⠷⠁⠀⠀
-⠀⠀⠀⠈⠊⠆⡃⠕⢕⢇⢇⢇⢇⢇⢏⢎⢎⢆⢄⠀⢑⣽⣿⢝⠲⠉⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⡿⠂⠠⠀⡇⢇⠕⢈⣀⠀⠁⠡⠣⡣⡫⣂⣿⠯⢪⠰⠂⠀⠀⠀⠀
-⠀⠀⠀⠀⡦⡙⡂⢀⢤⢣⠣⡈⣾⡃⠠⠄⠀⡄⢱⣌⣶⢏⢊⠂⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⢝⡲⣜⡮⡏⢎⢌⢂⠙⠢⠐⢀⢘⢵⣽⣿⡿⠁⠁⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠨⣺⡺⡕⡕⡱⡑⡆⡕⡅⡕⡜⡼⢽⡻⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⣼⣳⣫⣾⣵⣗⡵⡱⡡⢣⢑⢕⢜⢕⡝⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⣴⣿⣾⣿⣿⣿⡿⡽⡑⢌⠪⡢⡣⣣⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⡟⡾⣿⢿⢿⢵⣽⣾⣼⣘⢸⢸⣞⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠁⠇⠡⠩⡫⢿⣝⡻⡮⣒⢽⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-—————————————————————————————""")
+        print("""———————————No recivo?———————————""")
     else:
         print("gracias por su consulta")
+    
 
 def transferencias(div,cuentaT,sS,sP):
+    """
+     
+    """
     destino=int(input("ingrese la cuenta de destino: "))
     cuenta=int(input("que cuenta desea usar:\n1. pesos\n2. soles\n "))
     monto=float(input("ingrese el monto a retirar: "))
     valid=montoValido(div,cuenta,sS,sP,monto)
     if valid:
         sP,sS=restarCuenta(div,cuenta,sS,sP,monto)
-        if destino==cuentaT: 
+        if destino==cuentaT:
             print("transaccion realizada con exito")
         else: 
             print("transaccion fallida, su dinero sera reintegrado pasados 3 dias habiles")
     else: 
         print("no cuenta con fondos suficientes en la cuenta seleccionada, vuela a intentarlo")
-    nuevosaldo=[sP,sS]
-    return nuevosaldo
+    return sP,sS
 
 def montoValido(div,cuenta,sS,sP,op):
     """
-    montovalido(divisa, numero de cuenta, saldo cuenta 1, saldo cuenta 2, operacion)
-    Verifica que la cuenta espeficicada tenga saldo suficiente dependiendo de la operacion:
+    Verifica que la cuenta espeficicada tenga saldo suficiente dependiendo de la operacion:  
     Retorna:
         True si tiene saldo suficiente
         False no tiene saldo suficiente
@@ -108,6 +95,19 @@ def montoValido(div,cuenta,sS,sP,op):
             v=(sS-op)>=0
     return v
 def restarCuenta(div,cuenta,sS,sP,op):
+    """
+    Extrae una cantidad determinada de dinero en funcion de una divisa especificada.
+        div:    1 = Pesos
+                2 = Soles
+        cuenta:
+                1 = Cuenta en Pesos
+                2 = Cuenta en Soles
+        sS = Saldo de la cuenta en soles
+        sP = Saldo de la cuenta en Pesos
+        op = Saldo a restar
+    Retorna una tupla indicando los valores de saldo actualizados luego de la operacion.
+    
+    """
     if div<2:
         if cuenta<2:
             sP=(sP-op)
@@ -118,7 +118,6 @@ def restarCuenta(div,cuenta,sS,sP,op):
             sP=(sP-op/0.04194)
         else:
             sS=(sS-op)
-    nuevosaldo=[sP,sS]
-    return nuevosaldo
+    return sP,sS
 
 
